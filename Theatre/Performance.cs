18814c53797f5af6b65a2c68adc0690136e56242
Vasyl Don <chugunnabatareya@gmail.com>
2021-05-24 16:28:25 +0300

    Add project files.

---
+++ Theatre/Performance.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kursach_alpha
+{
+    public class Performance
+    {
+        public enum Genres : int
+        {
+            Comedy = 1,
+            Tragedy = 2,
+            Drama = 3,
+            Ballet = 4,
+            Musical = 5
+        }
+        public List<Tickets> Tickets = new List<Tickets>()
+        {
+            new Tickets(Kursach_alpha.Tickets.TicketsType.Parter),
+            new Tickets(Kursach_alpha.Tickets.TicketsType.Amphitheater),
+            new Tickets(Kursach_alpha.Tickets.TicketsType.Balcony)
+        };
+        static uint id;
+        public uint ID;
+        public string Author;
+        public string Name;
+        public Genres Genre;
+        public DateTime Date;
+        static Performance()
+        {
+            id = 0;
+        }
+        public Performance(string Author, string Name, Genres Genre, DateTime Date)
+        {
+            id += 1;
+            ID = id;
+            this.Author = Author;
+            this.Name = Name;
+            this.Genre = Genre;
+            this.Date = Date;
+        }
+        public void BuyTicket(int typeOfTickets, uint numberOfTickets)
+        {
+            Tickets[typeOfTickets - 1].Sell(numberOfTickets);
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine("\nName: " + Name);
+            Console.WriteLine("Author: " + Author);
+            Console.WriteLine("Genre: " + Genre);
+            Console.WriteLine("Date: " + Date.ToString("d"));
+            Console.WriteLine("Available tickets: ");
+            foreach (Tickets t in Tickets)
+                Console.WriteLine(t.TicketType + ": " + t.AvailableTickets + "\t\tPrice: " + t.Price);
+        }
+    }
+}
---
+++ Theatre/Tickets.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Kursach_alpha
+{
+    public class Tickets
+    {
+        public uint TotalNumberOfTickets;
+        public uint AvailableTickets
+        {
+            get { return TotalNumberOfTickets - ReservedTickets - SoldTickets; }
+            set { }
+        }
+        public uint ReservedTickets = 0;
+        public uint SoldTickets = 0;
+        public decimal Price { get; }
+        public enum TicketsType : int
+        {
+            Parter = 1,
+            Amphitheater = 2,
+            Balcony = 3
+        }
+        public TicketsType TicketType;
+
+        public Tickets(TicketsType ticketsType)
+        {
+            TicketType = ticketsType;
+            switch (ticketsType)
+            {
+                case TicketsType.Parter:
+                    TotalNumberOfTickets = 350;
+                    Price = 400;
+                    break;
+                case TicketsType.Amphitheater:
+                    TotalNumberOfTickets = 100;
+                    Price = 250;
+                    break;
+                case TicketsType.Balcony:
+                    TotalNumberOfTickets = 50;
+                    Price = 500;
+                    break;
+            }
+        }
+        public void Sell(uint numberOfTickets)
+        {
+            if (numberOfTickets > AvailableTickets)
+                throw new Exception("We don't have so many tickets");
+            else SoldTickets += numberOfTickets;
+        }
+        public void Reserve(uint numberOfTickets)
+        {
+            if (numberOfTickets > AvailableTickets)
+                throw new Exception("We don't have so many tickets");
+            else ReservedTickets += numberOfTickets;
+        }
+        public void SellReserved(uint numberOfTickets)
+        {
+            if (numberOfTickets > ReservedTickets)
+                throw new Exception("There are too few reserved tickets");
+            else
+            {
+                ReservedTickets -= numberOfTickets;
+                SoldTickets += numberOfTickets;
+            }
+        }
+    }
+}
